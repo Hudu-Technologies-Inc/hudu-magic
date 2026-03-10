44a32ce732a02e3a334b71cd464dae4ffcefa21d
Mason Stelter <masonville17@gmail.com>
2026-03-10 12:50:41 -0600

    updated naming and split into test file and non-test importable fixture

--- tests/integration/test_live.py
+++ tests/integration/test_live.py
@@ -1,33 +1,3 @@
-import os
-
-import pytest
-from dotenv import load_dotenv
-
-from hudu_magic import HuduClient
-from hudu_magic.endpoints import HuduEndpoint
-
-
-load_dotenv(".testenv")
-
-
-def _get_test_config():
-    api_key = os.getenv("HUDU_TEST_API_KEY")
-    instance_url = os.getenv("HUDU_TEST_INSTANCE")
-    return api_key, instance_url
-
-
-@pytest.fixture(scope="session")
-def integration_client():
-    api_key, instance_url = _get_test_config()
-
-    if not api_key or not instance_url:
-        pytest.skip("Integration test credentials not set in .testenv")
-
-    return HuduClient(
-        api_key=api_key,
-        instance_url=instance_url,
-    )
-
 def pytest_collection_modifyitems(config, items):
     run_integration = os.getenv("HUDU_RUN_INTEGRATION") == "1"
     skip_integration = pytest.mark.skip(reason="Set HUDU_RUN_INTEGRATION=1 to run integration tests")
